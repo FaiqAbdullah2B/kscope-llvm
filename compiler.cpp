f3ca216d2993ad9d8f38b8c53f50c6c09f5f210f
Faiq Abdullah <faiqabdullah2b@gmail.com>
2026-06-25 01:23:01 +0500

    One manga chapters worth of work done (20 minutes)

---
+++ compiler.cpp
@@ -0,0 +1,16 @@
+#include <string>
+
+enum Token {
+    tok_eof = -1,
+
+    // commands
+    tok_def = -2,
+    tok_extern = -3,
+
+    // primary
+    tok_identifier = -4,
+    tok_number = -5,
+};
+
+static std::string IdentifierStr; // filled in if tok identifier
+static double NumVal; // filled in if tok number
