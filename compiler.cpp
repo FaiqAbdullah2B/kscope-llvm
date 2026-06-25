88f5119655d2ad52fbeed0f5bd952a06e64739f2
Faiq Abdullah <faiqabdullah2b@gmail.com>
2026-06-25 12:42:30 +0500

    Completed the Lexer of the kscope tutorial

--- compiler.cpp
+++ compiler.cpp
@@ -14,3 +14,54 @@ enum Token {
 
 static std::string IdentifierStr; // filled in if tok identifier
 static double NumVal; // filled in if tok number
+
+/// gettok - Return the next token from standard input
+static int gettok() {
+    static int LastChar = ' ';
+
+    // skip any whitespace
+    while (isspace(LastChar))
+        LastChar = getchar();
+
+    if (isalpha(LastChar)) { // identifier: [a-zA-Z][a-zA-Z0-9]*
+        IdentifierStr = LastChar;
+        while (isalnum((LastChar = getchar())))
+            IdentifierStr += LastChar;
+
+        if (IdentifierStr == "def")
+            return tok_def;
+        if (IdentifierStr == "extern")
+            return tok_extern;
+        return tok_identifier;
+    }
+
+    if (isdigit(LastChar) || LastChar == '.') { // Number: [0-9.]+
+        std::string NumStr;
+        do {
+            NumStr += LastChar;
+            LastChar = getchar();
+        } while (isdigit(LastChar) || LastChar == '.');
+
+        NumVal = strtod(NumStr.c_str(), 0);
+        return tok_number;
+    }
+
+    if (LastChar == '#') {
+      // Comment until end of line.
+      do
+        LastChar = getchar();
+      while (LastChar != EOF && LastChar != '\n' && LastChar != '\r');
+
+      if (LastChar != EOF)
+        return gettok();
+    }
+
+    // Check for end of file, don't eat the EOF.
+    if (LastChar == EOF)
+        return tok_eof;
+
+    // Otherwise, just return the character as its ASCII value
+    int ThisChar = LastChar;
+    LastChar = getchar();
+    return ThisChar;
+}
